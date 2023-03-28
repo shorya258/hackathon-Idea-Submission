f009dd210bfe04d4e97d37e7508cd513622291a9
Shorya Hayaran <Shoryahayaran@gmail.com>
2023-03-29 00:02:19 +0530

    Added validation to HackathonForm.js

--- src/Components/HackathonForm.js
+++ src/Components/HackathonForm.js
@@ -19,7 +19,7 @@ export default function HackathonForm(props) {
   const [endDate, setEndDate] = useState("");
   const [gitLink, setGitLink] = useState("");
   const [otherLink, setOtherLink] = useState("");
-
+  const [errorsObj, setErrorsObj] = useState();
   // form is accessed when user clicks on Upload Submission on the first page AND when user clicks on edit option when a submission is expanded
   const handleSubmitHackathon = (e) => {
     e.preventDefault();
@@ -47,6 +47,9 @@ export default function HackathonForm(props) {
       otherLink,
       date: new Date(),
     };
+    if (!validateFormValues(newHackathonSubmission)) {
+      return;
+    }
     let storedSubmissions = JSON.parse(
       localStorage.getItem("storedSubmissions")
     );
@@ -66,6 +69,53 @@ export default function HackathonForm(props) {
     // after entering the submission details and pressing upload submission button, user is redirected to home
     history.push("/");
   };
+  const validateFormValues = (newHackathonSubmission) => {
+    const generatedErrors = {};
+    const regex =
+      /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+    if (newHackathonSubmission.title.trim().length === 0) {
+      generatedErrors.title = "Title can not be empty!";
+    } else if (newHackathonSubmission.title.trim().length < 4) {
+      generatedErrors.title = "Title should be at least four characters long!";
+    }
+    if (newHackathonSubmission.summary.trim().length === 0) {
+      generatedErrors.summary = "summary can not be empty!";
+    } else if (newHackathonSubmission.summary.trim().length < 4) {
+      generatedErrors.summary =
+        "summary should be at least four characters long!";
+    }
+    if (newHackathonSubmission.description.trim().length === 0) {
+      generatedErrors.description = "description can not be empty!";
+    }
+    if (newHackathonSubmission.hackathonName.trim().length === 0) {
+      generatedErrors.hackathonName = "Hackathon Name can not be empty!";
+    } else if (newHackathonSubmission.hackathonName.trim().length < 4) {
+      generatedErrors.hackathonName =
+        "Hackathon Name should be at least four characters long!";
+    }
+    if (newHackathonSubmission.coverImg === null) {
+      generatedErrors.coverImg = "Choose a cover image!";
+    }
+    if (newHackathonSubmission.gitLink.trim().length === 0) {
+      generatedErrors.gitLink = "GitHub Repository URL can not be empty!";
+    } else if (!regex.value(newHackathonSubmission.gitLink.trim())) {
+      generatedErrors.gitLink = "Enter a valid URL for GitHub Repository!";
+    }
+    if (newHackathonSubmission.startDate.length === 0) {
+      generatedErrors.startDate = "Start Date cannot not be empty!";
+    }
+    if (newHackathonSubmission.endDate.length === 0) {
+      generatedErrors.endDate = "End Date cannot not be empty!";
+    }
+    if (newHackathonSubmission.startDate > newHackathonSubmission.endDate) {
+      generatedErrors.startGreaterThanEndDate =
+        "Start Date should be less than End Date!";
+    }
+    // console.log(coverImg, typeof coverImg);
+    setErrorsObj(generatedErrors);
+    console.log(generatedErrors);
+  };
   //EDITING an hackathon submission detail
   const handleEditSubmission = () => {
     const editedHackathonSubmission = {
@@ -83,6 +133,9 @@ export default function HackathonForm(props) {
       otherLink,
       date,
     };
+    if (!validateFormValues(editedHackathonSubmission)) {
+      return;
+    }
     let storedSubmissions = JSON.parse(
       localStorage.getItem("storedSubmissions")
     );
@@ -149,6 +202,9 @@ export default function HackathonForm(props) {
               setTitle(e.target.value);
             }}
           />
+          {errorsObj !== undefined && errorsObj.title !== undefined && (
+            <legend className="error-dialog">{errorsObj.title}</legend>
+          )}
         </Box>
 
         <Box className="form-input summary">
@@ -163,12 +219,15 @@ export default function HackathonForm(props) {
               setSummary(e.target.value);
             }}
           />
+          {errorsObj !== undefined && errorsObj.summary !== undefined && (
+            <legend className="error-dialog">{errorsObj.summary}</legend>
+          )}
         </Box>
 
         <Box className="form-input description">
           <legend className="form-heading">Description</legend>
           <TextField
-            className="input-value"
+            className="input-value description-input"
             placeholder="Write a long description of your project. You can describe your idea and approach here."
             value={description}
             required={true}
@@ -177,7 +236,12 @@ export default function HackathonForm(props) {
               setDescription(e.target.value);
             }}
           />
-          <p className="character-length">0-300 characters</p>
+          {errorsObj !== undefined && errorsObj.description !== undefined && (
+            <legend className="error-dialog">{errorsObj.description}</legend>
+          )}
+          <p className="character-length">
+            {description.length}-3000 characters
+          </p>
         </Box>
 
         <Box className="form-input cover-img-wrapper">
@@ -197,12 +261,15 @@ export default function HackathonForm(props) {
               }}
             />
             <img
-              src={coverImg !== undefined ? coverImg : Upload}
+              src={coverImg === null ? Upload : coverImg}
               alt="upload button"
               className="cover-image"
               onClick={clickImageUpload}
             />
           </button>
+          {errorsObj !== undefined && errorsObj.coverImg !== undefined && (
+            <legend className="error-dialog">{errorsObj.coverImg}</legend>
+          )}
         </Box>
 
         <Box className="form-input hackthonName">
@@ -217,10 +284,13 @@ export default function HackathonForm(props) {
               setHackathonName(e.target.value);
             }}
           />
+          {errorsObj !== undefined && errorsObj.hackathonName !== undefined && (
+            <legend className="error-dialog">{errorsObj.hackathonName}</legend>
+          )}
         </Box>
         <Box className="form-input start-date">
           <legend className="form-heading">Hackathon Start date</legend>
-          <form className="form-input-date" noValidate>
+          <form className="form-input-date">
             <TextField
               id="date"
               label="Start Date"
@@ -230,15 +300,18 @@ export default function HackathonForm(props) {
               InputLabelProps={{
                 shrink: true,
               }}
-              onClick={(e) => {
+              onChange={(e) => {
                 setStartDate(e.target.value);
               }}
             />
           </form>
+          {errorsObj !== undefined && errorsObj.startDate !== undefined && (
+            <legend className="error-dialog">{errorsObj.startDate}</legend>
+          )}
         </Box>
         <Box className="form-input end-date">
           <legend className="form-heading">Hackathon End date</legend>
-          <form className="form-input-date" noValidate>
+          <form className="form-input-date">
             <TextField
               id="date"
               label="End Date"
@@ -248,12 +321,24 @@ export default function HackathonForm(props) {
               InputLabelProps={{
                 shrink: true,
               }}
-              onClick={(e) => {
+              onChange={(e) => {
                 setEndDate(e.target.value);
               }}
             />
           </form>
+
+          {errorsObj !== undefined && errorsObj.endDate !== undefined && (
+            <legend className="error-dialog">{errorsObj.endDate}</legend>
+          )}
+
+          {errorsObj !== undefined &&
+            errorsObj.startGreaterThanEndDate !== undefined && (
+              <legend className="error-dialog">
+                {errorsObj.startGreaterThanEndDate}
+              </legend>
+            )}
         </Box>
+
         <Box className="form-input github-link">
           <legend className="form-heading">GitHub Repository</legend>
           <TextField
@@ -268,6 +353,9 @@ export default function HackathonForm(props) {
               setGitLink(e.target.value);
             }}
           />
+          {errorsObj !== undefined && errorsObj.gitLink !== undefined && (
+            <legend className="error-dialog">{errorsObj.gitLink}</legend>
+          )}
         </Box>
         <Box className="form-input other-link">
           <legend className="form-heading">Other Links</legend>
